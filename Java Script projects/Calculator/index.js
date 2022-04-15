ae1a91efd19c42ab55edd3cdeaa9a0fcd7a2750a
aishwarya524 <103186648+aishwarya524@users.noreply.github.com>
2022-04-16 00:05:46 +0530

    Update index.js

--- Java Script projects/Calculator/index.js
+++ Java Script projects/Calculator/index.js
@@ -1,24 +1,51 @@
 var displayingValue = document.getElementById("display")
 
+function isOperator(buttonValue){
+    if(buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/' || buttonValue === '%'){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
 function selectedButton(buttonValue){
     if(buttonValue ==="CLR"){
-        displayingValue.innerText = ''
+        displayingValue.innerText = '0'
     }
     else if(buttonValue === 'b'){
-        displayingValue.innerText = displayingValue.innerText.substring(0,displayingValue.innerText.length-1)
+        displayingValue.innerText = displayingValue.innerText.slice(0,-1)
     }
     else if(buttonValue === '='){
-        let newValue = displayingValue.innerText
-        if(newValue!=""){
-            displayingValue.innerText = eval(newValue)
-        }
+            if(isOperator(displayingValue.innerText.slice(-1))){
+                while(isOperator(displayingValue.innerText.slice(-1))){
+                    displayingValue.innerText = displayingValue.innerText.slice(0,-1)
+                }
+            }
+            displayingValue.innerText = eval(displayingValue.innerText)
     }
     else{
         if(displayingValue.innerText === '0'){
-            displayingValue.innerText = buttonValue
+            if(buttonValue != '00'){
+                if(buttonValue === '.' || isOperator(buttonValue)){
+                    displayingValue.innerText = displayingValue.innerText + buttonValue
+                }
+                else{
+                    displayingValue.innerText = buttonValue
+                }                
+            }
         }
         else{
-            displayingValue.innerText = displayingValue.innerText + buttonValue
+            let i = displayingValue.innerText.length
+            if(buttonValue === '.' && displayingValue.innerText.substring(i-2,i)==="00"){
+                displayingValue.innerText = displayingValue.innerText.slice(0,-1) + buttonValue
+            }
+            else if(isOperator(displayingValue.innerText[i-1]) && isOperator(buttonValue)){
+                displayingValue.innerText = displayingValue.innerText.slice(0,-1) + buttonValue
+            }
+            else{
+                displayingValue.innerText = displayingValue.innerText + buttonValue
+            }            
         }
     }    
 }
