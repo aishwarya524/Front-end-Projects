83a4745496fd2298e1ba6c8d8f73cce431e2e791
aishwarya524 <103186648+aishwarya524@users.noreply.github.com>
2022-04-15 20:45:38 +0530

    Create index.js

---
+++ Java Script projects/Calculator/index.js
@@ -0,0 +1,19 @@
+var displayingValue = document.getElementById("display")
+
+function selectedButton(buttonValue){
+    if(buttonValue ==="CLR"){
+        displayingValue.innerText = ''
+    }
+    else if(buttonValue === 'b'){
+        displayingValue.innerText = displayingValue.innerText.substring(0,displayingValue.innerText.length-1)
+    }
+    else if(buttonValue === '='){
+        let newValue = displayingValue.innerText
+        if(newValue!=""){
+            displayingValue.innerText = eval(newValue)
+        }
+    }
+    else{
+        displayingValue.innerText = displayingValue.innerText + buttonValue
+    }    
+}
